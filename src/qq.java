663bb7249a2363668143cd0dbd540e23f3135aab
dzt666 <18666735017@163.com>
2022-05-03 17:38:54 +0800

    11

---
+++ src/qq.java
@@ -0,0 +1,20 @@
+public class qq {
+    public static void main(String[] args) {
+        System.out.print("");
+        System.out.print("");
+        System.out.print("");
+        System.out.print("");
+        System.out.print("11");
+        System.out.print("11");
+        System.out.print("11");
+        System.out.print("11");
+        System.out.print("11");
+        System.out.print("11");
+        System.out.print("11");
+        System.out.print("11");
+    }
+
+    private void qq1() {
+        System.out.printf("23232   ");
+    }
+}
